131183ca785512ada97fad86e79e58161af0c36e
roronoa52 <farhan.yudha2016we@gmail.com>
2024-06-03 08:04:41 +0700

    fix get all banks

--- app/api/v1/banks/router.js
+++ app/api/v1/banks/router.js
@@ -6,7 +6,7 @@ const { authenticateUser } = require("../../../middlewares/auth")
 router.post ("/banks", authenticateUser, create)
 router.put ("/banks/:id", authenticateUser, update)
 router.delete ("/banks/:id", authenticateUser, destroy)
-router.get ("/banks", authenticateUser, index)
+router.get ("/banks", index)
 router.get ("/banks/:id", authenticateUser, find)
 
 module.exports = router
